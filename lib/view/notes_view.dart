faa25f1b62174db734f69452aaf5de64be2f094c
zicoovic <abdelrhmanzakariaqa@gmail.com>
2023-01-22 08:52:10 +0300

    update theme and create notes view

---
+++ lib/view/notes_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
