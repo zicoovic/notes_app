35f9a49f60dd0de50d3f8fc4fdb691a7f3752464
zicoovic <abdelrhmanzakariaqa@gmail.com>
2023-01-25 09:21:55 +0300

    display notes

--- lib/view/widgets/notes_list_view.dart
+++ lib/view/widgets/notes_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 
 import 'custom_note_item.dart';
 
@@ -7,18 +10,23 @@ class NotesListView extends StatelessWidget {
   final data = const [];
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        itemCount: 100,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.all(4.0),
-            child: NoteItem(),
-          );
-        },
-      ),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: ListView.builder(
+            padding: EdgeInsets.zero,
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              return const Padding(
+                padding: EdgeInsets.all(4.0),
+                child: NoteItem(),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
